6f141a189323a41b085eec4587643b833ddcc70c
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-10 23:24:49 -0800

    seed.py: remove User from imported models and User related code

--- server/seed.py
+++ server/seed.py
@@ -8,7 +8,7 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User, Movie, TVShow
+from models import db, Movie, TVShow
 
 if __name__ == '__main__':
     fake = Faker()
@@ -18,6 +18,5 @@ if __name__ == '__main__':
 
         # Delete previous records
         print("deleting previous records...")
-        User.query.delete()
         Movie.query.delete()
         TVShow.query.delete()
