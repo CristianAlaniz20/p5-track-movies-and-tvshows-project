3ffeb1f7c02ab96b529723171121f658a791e85e
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-12 14:45:46 -0800

    app.py: Signup resource(remove assiging session['user_id'] code) Login resource(create resource) and import no_user_found_response

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ from config import app, db, api
 # Add your model imports
 from models import User, Movie, TVShow, MovieWatchEvent, TVShowWatchEvent
 # Helper functions imports
-from helpers import no_data_response, empty_username_or_password_response, error_response
+from helpers import no_data_response, empty_username_or_password_response, error_response, no_user_found_response
 
 # Views go here!
 
@@ -36,9 +36,7 @@ class Signup(Resource):
                 db.session.add(new_user)
                 db.session.commit()
 
-                # assign new_user db instance id to session user_id
-                session['user_id'] = new_user.id
-
+                # Create response
                 response = {
                     "message" : "User successfully created!",
                     "new_user" : new_user.to_dict()
@@ -54,8 +52,44 @@ class Signup(Resource):
         except Exception as e:
             return error_response(e)
 
+class Login(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data['username']
+            password = data['password']
+            user = User.query.filter_by(username=username).first()
+
+            if not data:
+                return no_data_response()
+            elif len(username) == 0 or len(password) == 0:
+                return empty_username_or_password_response()
+            elif not user:
+                return no_user_found_response()
+            else:
+                if user.authenticate(password):
+                    # assign user db id to session user_id
+                    session['user_id'] = new_user.id
+
+                    # create response
+                    response = {
+                        "message" : "Successfully logged in.",
+                        "user" : user.to_dict()
+                    }
+
+                    return make_response(jsonify(response), 200)
+
+                # invalid password
+                return make_response(jsonify({"error" : "invalid password."}), 404)
+
+        # All other exceptions
+        except Exception as e:
+            return error_response(e)
+        
+
 # Adding resources to api
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
 
 
 if __name__ == '__main__':
