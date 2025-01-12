201fbdac900271121fe408447bf6504fa3655efa
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-12 14:10:24 -0800

    helpers.py: create file and initial helpers

---
+++ server/helpers.py
@@ -0,0 +1,12 @@
+# Remote library imports
+from flask import jsonify, make_response
+
+# Helper functions
+def no_data_response():
+    return make_response(jsonify({"error" : "No data was received."}), 404)
+
+def empty_username_or_password_response():
+    return make_response(jsonify({"error" : "username and password cannot be empty."}), 404)
+
+def error_response(error):
+    return make_response(jsonify({"error": str(error)}), 404)
