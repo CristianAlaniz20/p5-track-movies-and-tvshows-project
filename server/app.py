249d2e86f0094f10391382ebf49acfe34e8a1b9b
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-13 19:25:39 -0800

    app.py: create SearchResults resource

--- server/app.py
+++ server/app.py
@@ -111,11 +111,51 @@ class Logout(Resource):
         else:
             return make_response(jsonify({"error" : "Already logged out"}), 404)
 
+class SearchResults(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            title = data['title']
+
+            if not data:
+                return no_data_response()
+            elif len(title) == 0:
+                return make_response(jsonify({"error" : "Enter a title before searching."}), 404)
+            else:
+                # Query Movie and TVShow db table for content that matches the title received
+                movie_results = [movie.to_dict() for movie in Movie.query.all() if title.lower() in movie.title.lower()]
+                tv_show_results = [tv_show.to_dict() for tv_show in TVShow.query.all() if title.lower() in tv_show.title.lower()]
+
+                search_results = movie_results + tv_show_results
+
+                # Create response
+                if len(search_results) > 0:
+                    # reults found response
+                    response = {
+                        "message" : "Matching results found.",
+                        "results" : search_results
+                    }
+                else:
+                    # no results found response
+                    response = {
+                        "message" : "No results found for the given string.",
+                        "results" : search_results
+                    }
+
+                return make_response(jsonify(response), 200)
+
+        # All other exceptions
+        except Exception as e:
+            return error_response(e)
+
+
 # Adding resources to api
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(SearchResults, '/search_results', endpoint='search_results')
+
 
 
 if __name__ == '__main__':
