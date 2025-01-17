018dc3c0098f6cb077ed5b6b6d4f491fe23c703c
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-16 19:10:35 -0800

    Header.js: Create Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>Track Movies and TV Shows</h1>
+            <h2>The one stop shop to track all the movies and tv shows that you have watched or would like to watch!</h2>
+        </div>
+    )
+}
+
+export default Header
