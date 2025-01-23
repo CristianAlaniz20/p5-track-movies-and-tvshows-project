ddc0d5343c266c142876e0b03ffb94e05511cbeb
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-22 23:20:50 -0800

    App.js: create isLoggedIn state, import LoginPage, and use condtional code to render LoginPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  if (!isLoggedIn) {
+    return <LoginPage setIsloggedIn={setIsLoggedIn} />
+  }
+  
   return <h1>Project Client</h1>;
 }
 
