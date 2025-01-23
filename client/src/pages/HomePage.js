8efe04e5d355905e98c9de780d54a106f6555739
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-22 23:50:41 -0800

    HomePage.js: create HomePage component

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <SearchBar />
+            {/*<SearchResults /> */}
+        </div>
+    )
+}
+
+export default HomePage
