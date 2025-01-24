bd20ec594c70b37a525492a063dc25dec01daa33
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-23 23:08:45 -0800

    HomePage.js: create results state and pass value and setter function as props

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import SearchResults from "../components/SearchResults";
 
 function HomePage() {
+    // search results state
+    const [results, setResults] = useState(null)
+
     return (
         <div>
             <Header />
-            <SearchBar />
-            {/*<SearchResults /> */}
+            <SearchBar setResults={setResults} />
+            {/* Checks if anything has been searched */}
+            {results ? (
+                <SearchResults results={results} />
+            ) : (
+                <h4>Search for any show you desire!</h4>
+            )}
         </div>
     )
 }
