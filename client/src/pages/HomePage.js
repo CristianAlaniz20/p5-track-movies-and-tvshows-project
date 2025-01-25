1aef470a2f796e6bb5d96c23a193de5afd3d187e
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-24 18:55:12 -0800

    HomePage.js: add search results filtration

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,14 +6,40 @@ import SearchResults from "../components/SearchResults";
 function HomePage() {
     // search results state
     const [results, setResults] = useState(null)
+    // filter selection state
+    const [filter, setFilter] = useState("all")
+
+    // sets the filter every time a different option is selected
+    function handleSelectChange(event) {
+        setFilter(event.target.value)
+    }
+
+    // filters the results 
+    const filteredResults = results ? results.filter(result => {
+        switch (filter) {
+            case "movies":
+                return !!result.duration
+            case "tv shows":
+                return !!result.seasons
+            default:
+                return true
+        }
+    }) : [];
 
     return (
         <div>
             <Header />
             <SearchBar setResults={setResults} />
+            {/* dropdown for filtration */}
+            <label htmlFor="search-filter">Filter: </label>
+            <select name="search-filter" value={filter} onChange={handleSelectChange}>
+                <option value="all">all</option>
+                <option value="movies">movies</option>
+                <option value="tv shows">tv shows</option>
+            </select>
             {/* Checks if anything has been searched */}
             {results ? (
-                <SearchResults results={results} />
+                <SearchResults results={filteredResults} />
             ) : (
                 <h4>Search for any show you desire!</h4>
             )}
