25d2a1317f82dd756f815d850bece9583ef1c6a7
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-27 17:33:25 -0800

    TVShowDetails.js: add button that navigates to CreateTVShowWatchEvent

--- client/src/pages/TVShowDetails.js
+++ client/src/pages/TVShowDetails.js
@@ -1,9 +1,10 @@
 import React,  { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import ContentDetails from "../components/ContentDetails";
 
 function TVShowDetails() {
     const { tv_show_id } = useParams() // Access the tv_show_id from the route
+    const history = useHistory() // re routes
     const [tvshow, setTVShow] = useState(null) // movie state
 
     useEffect(() => {
@@ -20,14 +21,20 @@ function TVShowDetails() {
         .catch(error => console.error(error))
     }, [tv_show_id])
 
+    // re routes to CreatTVShowWatchEvent component
+    const handleAddToWatchlistClick = () => history.push(`/add_to_tv_show_list/${tvshow.id}`)
+
     return (
         <div>
             {/* conditionally render tvshow details after loading */}
             {tvshow ? (
-                <ContentDetails 
-                    contentObj={tvshow}
-                    jsx={<p>Seasons: {tvshow.seasons}</p>}
-                />
+                <>
+                    <ContentDetails 
+                        contentObj={tvshow}
+                        jsx={<p>Seasons: {tvshow.seasons}</p>}
+                    />
+                    <button onClick={handleAddToWatchlistClick} >Add To Watchlist</button>
+                </>
             ) : (
                 <h1>TV Show is loading...</h1>
             )}
