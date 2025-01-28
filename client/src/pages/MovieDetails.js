b4a3177286f6b9b4d08434428692e36efc0a4f0c
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-27 17:32:58 -0800

    MovieDetails.js: add button that navigates to CreateMovieWatchEvent

--- client/src/pages/MovieDetails.js
+++ client/src/pages/MovieDetails.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { convertMinutesToHoursAndMinutes } from "../helpers";
 import ContentDetails from "../components/ContentDetails";
 
 function MovieDetails() {
     const { movie_id } = useParams() // Access the movie_id from the route
+    const history = useHistory() // re routes
     const [movie, setMovie]  = useState(null) // movie state
 
     useEffect(() => {
@@ -21,14 +22,20 @@ function MovieDetails() {
         .catch(error => console.error(error))
     }, [movie_id])
 
+    // re routes to CreateMovieWatchEvent component
+    const handleAddToWatchlistClick = () => history.push(`/add_to_movie_list/${movie.id}`)
+
     return (
         <div>
             {/* conditionally render movie details after movie response */}
             {movie ? (
-                <ContentDetails 
-                    contentObj={movie}
-                    jsx={<p>Duration: {convertMinutesToHoursAndMinutes(movie.duration)}</p>}
-                />
+                <>
+                    <ContentDetails 
+                        contentObj={movie}
+                        jsx={<p>Duration: {convertMinutesToHoursAndMinutes(movie.duration)}</p>}
+                    />
+                    <button onClick={handleAddToWatchlistClick} >Add To Watchlist</button>
+                </>
             ) : (
                 <h1>Movie is loading...</h1>
             )}
