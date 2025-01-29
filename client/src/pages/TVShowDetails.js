3eafe6a92607173b44f49aa61f8315f2135e3b31
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-29 09:41:23 -0800

    TVShowDetails.js: completely renovate file introducing ContentContext state and WatchEventsContext states. Removed direct API call and list a users tv show watch history.

--- client/src/pages/TVShowDetails.js
+++ client/src/pages/TVShowDetails.js
@@ -1,39 +1,46 @@
-import React,  { useEffect, useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
 import ContentDetails from "../components/ContentDetails";
+import { ContentContext } from "../contexts/ContentContext";
+import { WatchEventsContext } from "../contexts/WatchEventsContext";
+import ListWatchEvents from "../components/ListWatchEvents";
 
 function TVShowDetails() {
-    const { tv_show_id } = useParams() // Access the tv_show_id from the route
-    const history = useHistory() // re routes
-    const [tvshow, setTVShow] = useState(null) // movie state
+    const { currentTVShow } = useContext(ContentContext) // currentMovie state from ContentContext)
+    // states and method from WatchEventsContext
+    const { watchedEvents, watchlistEvents, retrieveTVShowEvents } = useContext(WatchEventsContext)
+
+    // empty watch event list messsage
+    const emptyWatchEventListMessage = "No watch events found for this list."
 
     useEffect(() => {
-        // GET request to backend TVShowResource
-        fetch(`/tv_show/${tv_show_id}`)
-        .then(res => {
-            if (res.status === 200) {
-                return res.json()
-            } else {
-                console.log("Error fetching tv show")
-            }
-        })
-        .then(resTVShow => setTVShow(resTVShow.tv_show))
-        .catch(error => console.error(error))
-    }, [tv_show_id])
-
-    // re routes to CreatTVShowWatchEvent component
-    const handleAddToWatchlistClick = () => history.push(`/add_to_tv_show_list/${tvshow.id}`)
+        // retrieve tv show events until currentTVShow has a value
+        if (currentTVShow) {
+            retrieveTVShowEvents(currentTVShow)
+        }
+    }, [currentTVShow])
 
     return (
         <div>
-            {/* conditionally render tvshow details after loading */}
-            {tvshow ? (
+            {/* conditionally render tvshow details */}
+            {currentTVShow && watchedEvents && watchlistEvents ? (
                 <>
-                    <ContentDetails 
-                        contentObj={tvshow}
-                        jsx={<p>Seasons: {tvshow.seasons}</p>}
-                    />
-                    <button onClick={handleAddToWatchlistClick} >Add To Watchlist</button>
+                    {/* Displays content details */}
+                    <div>
+                        <ContentDetails 
+                            contentObj={currentTVShow}
+                            jsx={<p>Seasons: {currentTVShow.seasons}</p>}
+                        />
+                    </div>
+                    {/* Displays watched events */}
+                    <div>
+                        <h2>Watched Events</h2>
+                        <ListWatchEvents eventsList={watchedEvents} emptyWatchEventListMessage={emptyWatchEventListMessage} />
+                    </div>
+                    {/* Displays watched events */}
+                    <div>
+                        <h2>Watchlist Events</h2>
+                        <ListWatchEvents eventsList={watchlistEvents} emptyWatchEventListMessage={emptyWatchEventListMessage} />
+                    </div>
                 </>
             ) : (
                 <h1>TV Show is loading...</h1>
