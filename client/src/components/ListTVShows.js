fa971dd5fef21011c422448b29f63f425cdc108f
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-29 01:03:25 -0800

    ListTVShow.js: create ListTVShows component

---
+++ client/src/components/ListTVShows.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { ContentContext } from "../contexts/ContentContext";
+
+function ListTVShows({ tvShowList, emptyContentListMessage }) {
+    const { setCurrentTVShow } = useContext(ContentContext) // set current tv show function from ContentContext
+
+    const history = useHistory() // for navigation purposes
+
+    // handle tv show poster being clicked
+    const handleTVShowContentClick = (show) => {
+        setCurrentTVShow(show)
+        history.push("/tv_show_details")
+    }
+
+    return (
+        <div>
+            {/* lists a tv show poster for every tv show in tvShowList */}
+            {tvShowList.length > 0 ? tvShowList.map(show => {
+                return (
+                    <img 
+                        key={show.id}
+                        src={show.poster_url} 
+                        alt={`${show.title} poster`} 
+                        onClick={() => handleTVShowContentClick(show)} 
+                    />
+                )
+            }) : <p>{emptyContentListMessage}</p>}
+        </div>
+    )
+}
+
+export default ListTVShows
