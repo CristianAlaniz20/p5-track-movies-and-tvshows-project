50bc5bd30e5f90bfa1e620906a51d2186ad4922b
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-29 00:16:58 -0800

    UserDashboard.js: add conditional rendering checking if each list has any length. If not show emptyContentListMessage.

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -5,35 +5,37 @@ function UserDashboard() {
     // watched and watchlist lists for movies and tv shows
     const { watchedMovies, watchlistMovies, watchedTVShows, watchlistTVShows } = useContext(ContentStatusContext)
     
+    const emptyContentListMessage = "No content found for this list. Go to Search tab to add content."
+    
     return (
         <div>
             <div>
                 {/* Display watched movies */}
                 <h2>Watched Movies</h2>
-                {watchedMovies.map(movie => {
+                {watchedMovies.length > 0 ? watchedMovies.map(movie => {
                     return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
-                })}
+                }) : <p>{emptyContentListMessage}</p> }
             </div>
             <div>
                 {/* Display watchlist movies */}
                 <h2>Watchlist Movies</h2>
-                {watchlistMovies.map(movie => {
+                {watchlistMovies.length > 0 ? watchlistMovies.map(movie => {
                     return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
-                })}
+                }) : <p>{emptyContentListMessage}</p>}
             </div>
             <div>
                 {/* Display watched tv shows */}
                 <h2>Watched TV Shows</h2>
-                {watchedTVShows.map(show => {
+                {watchedTVShows.length > 0 ? watchedTVShows.map(show => {
                     return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
-                })}
+                }) : <p>{emptyContentListMessage}</p>}
             </div>
             <div>
                 {/* Display watchlist tv shows */}
                 <h2>Watchlist TV Shows</h2>
-                {watchlistTVShows.map(show => {
+                {watchlistTVShows.length > 0 ? watchlistTVShows.map(show => {
                     return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
-                })}
+                }) : <p>{emptyContentListMessage}</p>}
             </div>
         </div>
     )
