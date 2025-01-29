a6599278e337fd0c0ef1d2ef873e6c922ffb0d1e
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-28 23:54:31 -0800

    UserDashboard.js: create UserDashboard component

---
+++ client/src/pages/UserDashboard.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { ContentStatusContext } from "../contexts/ContentStatusContext";
+
+function UserDashboard() {
+    // watched and watchlist lists for movies and tv shows
+    const { watchedMovies, watchlistMovies, watchedTVShows, watchlistTVShows } = useContext(ContentStatusContext)
+    
+    return (
+        <div>
+            <div>
+                {/* Display watched movies */}
+                <h2>Watched Movies</h2>
+                {watchedMovies.map(movie => {
+                    return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
+                })}
+            </div>
+            <div>
+                {/* Display watchlist movies */}
+                <h2>Watchlist Movies</h2>
+                {watchlistMovies.map(movie => {
+                    return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
+                })}
+            </div>
+            <div>
+                {/* Display watched tv shows */}
+                <h2>Watched TV Shows</h2>
+                {watchedTVShows.map(show => {
+                    return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
+                })}
+            </div>
+            <div>
+                {/* Display watchlist tv shows */}
+                <h2>Watchlist TV Shows</h2>
+                {watchlistTVShows.map(show => {
+                    return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default UserDashboard
