cb589452da254e65a5c4e43cb8f4170403afcc98
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-28 17:37:51 -0800

    AppContext.js: create context file

---
+++ client/src/contexts/AppContext.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { UserProvider } from "./UserContext";
+import { WatchEventsProvider } from "./WatchEventsContext";
+import { ContentProvider } from "./ContentContext";
+
+function AppProvider({ children }) {
+    return (
+        <UserProvider>
+            <WatchEventsProvider>
+                <ContentProvider>
+                    {children}
+                </ContentProvider>
+            </WatchEventsProvider>
+        </UserProvider>
+    )
+}
+
+export default AppProvider
