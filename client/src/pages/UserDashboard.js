596ecd3a7aae8194775c38328411e606174d9ce7
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-29 01:05:28 -0800

    UserDashboard.js: extract movie and tv show listing code to ListMovies and ListTVShows. Import and use components.

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import { ContentStatusContext } from "../contexts/ContentStatusContext";
+import ListMovies from "../components/ListMovies";
+import ListTVShows from "../components/ListTVShows";
 
 function UserDashboard() {
-    // watched and watchlist lists for movies and tv shows
+    // watched and watchlist lists for movies and tv shows from ContentStatusContext
     const { watchedMovies, watchlistMovies, watchedTVShows, watchlistTVShows } = useContext(ContentStatusContext)
-    
+    // empty content list message
     const emptyContentListMessage = "No content found for this list. Go to Search tab to add content."
     
     return (
@@ -12,30 +14,25 @@ function UserDashboard() {
             <div>
                 {/* Display watched movies */}
                 <h2>Watched Movies</h2>
-                {watchedMovies.length > 0 ? watchedMovies.map(movie => {
-                    return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
-                }) : <p>{emptyContentListMessage}</p> }
+                <ListMovies movieList={watchedMovies} emptyContentListMessage={emptyContentListMessage} />
             </div>
+
             <div>
                 {/* Display watchlist movies */}
                 <h2>Watchlist Movies</h2>
-                {watchlistMovies.length > 0 ? watchlistMovies.map(movie => {
-                    return <img src={movie.poster_url} alt={`${movie.title} poster`} key={movie.id} />
-                }) : <p>{emptyContentListMessage}</p>}
+                <ListMovies movieList={watchlistMovies} emptyContentListMessage={emptyContentListMessage} />
             </div>
+            
             <div>
                 {/* Display watched tv shows */}
                 <h2>Watched TV Shows</h2>
-                {watchedTVShows.length > 0 ? watchedTVShows.map(show => {
-                    return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
-                }) : <p>{emptyContentListMessage}</p>}
+                <ListTVShows tvShowList={watchedTVShows} emptyContentListMessage={emptyContentListMessage} />
             </div>
+
             <div>
                 {/* Display watchlist tv shows */}
                 <h2>Watchlist TV Shows</h2>
-                {watchlistTVShows.length > 0 ? watchlistTVShows.map(show => {
-                    return <img src={show.poster_url} alt={`${show.title} poster`} key={show.id} />
-                }) : <p>{emptyContentListMessage}</p>}
+                <ListTVShows tvShowList={watchlistTVShows} emptyContentListMessage={emptyContentListMessage} />
             </div>
         </div>
     )
