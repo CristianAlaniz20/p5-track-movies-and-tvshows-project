c249a5e5ed0f439e0a7677f7a7d4ac236b77716b
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-29 23:43:16 -0800

    Content.js: import seCurrentMovie and setCurrentTVShow functions from ContentContext and integrate them into code

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
+import { ContentContext } from "../contexts/ContentContext";
 
 function Content({ result }) {
     const history = useHistory()
+    const { setCurrentMovie, setCurrentTVShow } = useContext(ContentContext)
 
     // reroute to MovieDetails component
-    const handleMovieImageClick = () => history.push(`/movie/${result.id}`)
+    const handleMovieImageClick = (result) => {
+        setCurrentMovie(result)
+        history.push(`/movies/${result.id}`)
+    }
     // reroute to TVShowDetails component
-    const handleTVShowImageClick = () => history.push(`/tv_show/${result.id}`)
+    const handleTVShowImageClick = (result) => {
+        setCurrentTVShow(result)
+        history.push(`/tv_shows/${result.id}`)
+    }
 
     return (
         <>
             {/* checks if result has a duration attribute to differentiate between a Movie and TV Show */}
             {result.duration ? (
-                <img src={result.poster_url} alt={`${result.title} poster`} onClick={handleMovieImageClick} />
+                <img src={result.poster_url} alt={`${result.title} poster`} onClick={() => handleMovieImageClick(result)} />
             ) : (
-                <img src={result.poster_url} alt={`${result.title} poster`} onClick={handleTVShowImageClick} />
+                <img src={result.poster_url} alt={`${result.title} poster`} onClick={() => handleTVShowImageClick(result)} />
             )}
         </>
     )
