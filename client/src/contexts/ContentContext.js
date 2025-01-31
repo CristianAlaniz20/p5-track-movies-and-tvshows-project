763b732c861011b44050dcac5cf941e6059b68e7
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-31 11:34:38 -0800

    remove checkTypeAndSetContent function

--- client/src/contexts/ContentContext.js
+++ client/src/contexts/ContentContext.js
@@ -1,32 +1,13 @@
-import React, { createContext, useState, useContext } from 'react';
-import { WatchEventsContext } from './WatchEventsContext';
+import React, { createContext, useState } from 'react';
 
 export const ContentContext = createContext();
 
 export const ContentProvider = ({ children }) => {
-    const [searchResults, setSearchResults] = useState([]); // search results state
-    const [currentContent, setCurrentContent] = useState({}) // current content state
-    const { setWatchEvents } = useContext(WatchEventsContext) // setter function from WatchEventsContext
+    const [searchResults, setSearchResults] = useState([]);
+    const [currentContent, setCurrentContent] = useState(null);
 
-    // sets currentContent state and sets WatchEvents state based on the type of content received
-    const checkTypeAndSetContent = (contentObj) => {
-        // checks if contentObj is a movie
-        if (contentObj.duration) {
-            setCurrentContent(contentObj)
-            setWatchEvents(contentObj.movie_watch_events)
-        // checks if contentObj is a tv show
-        } else if (contentObj.seasons) {
-            setCurrentContent(contentObj)
-            setWatchEvents(contentObj.tv_show_watch_events)
-        } else {
-            console.warn("content is missing an attribute", contentObj)
-        }
-    }
-
-    // searches Movie and TV Show database for any content matching a title
     const searchContent = async (title) => {
         try {
-            // search for movies and tv shows that match title 
             const response = await fetch('/search_results', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -43,13 +24,9 @@ export const ContentProvider = ({ children }) => {
     };
 
     return (
-        <ContentContext.Provider value={{
-            searchResults,
-            searchContent,
-            currentContent,
-            checkTypeAndSetContent,
-        }}>
+        <ContentContext.Provider value={{ searchResults, searchContent, currentContent, setCurrentContent }}>
             {children}
         </ContentContext.Provider>
     );
 };
+
