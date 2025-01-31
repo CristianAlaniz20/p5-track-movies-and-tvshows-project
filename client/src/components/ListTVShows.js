b433c5dae96cd877038118ea38b4fa837658d7b7
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-31 12:24:20 -0800

    ListTVShows.js: use checkTypeAndSetContent and setWatchEvents

--- client/src/components/ListTVShows.js
+++ client/src/components/ListTVShows.js
@@ -1,32 +1,38 @@
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { ContentContext } from "../contexts/ContentContext";
+import { WatchEventsContext } from "../contexts/WatchEventsContext";
 
 function ListTVShows({ tvShowList }) {
     const { checkTypeAndSetContent } = useContext(ContentContext) // set current tv show function from ContentContext
+    const { setWatchEvents } = useContext(WatchEventsContext)
 
     const history = useHistory() // for navigation purposes
 
     // handle tv show poster being clicked
     const handleTVShowContentClick = (show) => {
-        checkTypeAndSetContent(show)
+        const events = checkTypeAndSetContent(show)
+        setWatchEvents(events)
         history.push(`/tv_shows/${show.id}`)
     }
 
     return (
         <div>
-            {/* lists a tv show poster for every tv show in tvShowList */}
-            {tvShowList.length > 0 ? tvShowList.map(show => {
-                return (
-                    <img 
-                        key={show.id}
-                        src={show.poster_url} 
-                        alt={`${show.title} poster`} 
-                        onClick={() => handleTVShowContentClick(show)} 
-                    />
-                )
-            }) : <p>No content found for this list. Go to Search Database tab to add content.</p>}
-        </div>
+        {/* Lists a movie poster for every movie in movieList */}
+        {tvShowList.length > 0 ? (
+            tvShowList.map(show => (
+                <img 
+                    key={show.id} 
+                    src={show.poster_url} 
+                    alt={`${show.title} poster`} 
+                    onClick={() => handleMovieContentClick(show)} 
+                    style={{ cursor: "pointer" }}
+                />
+            ))
+        ) : (
+            <p>No content found for this list. Go to Search Database tab to add content.</p>
+        )}
+    </div>
     )
 }
 
