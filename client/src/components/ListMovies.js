4c7e5cf9337d3d5e9027ac37e6e36a698a7edf95
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-31 12:21:11 -0800

    ListMovies.js: use checkTypeAndSetContent and setWatchEvents

--- client/src/components/ListMovies.js
+++ client/src/components/ListMovies.js
@@ -1,33 +1,37 @@
 import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom"; // Changed from useHistory (deprecated)
 import { ContentContext } from "../contexts/ContentContext";
+import { WatchEventsContext } from "../contexts/WatchEventsContext"; // Added WatchEventsContext
 
 function ListMovies({ movieList }) {
-    const { checkTypeAndSetContent } = useContext(ContentContext) // set current movie function from ContentContext
+    const { checkTypeAndSetContent } = useContext(ContentContext); // Set current movie in ContentContext
+    const { setWatchEvents } = useContext(WatchEventsContext); // Update global watch events
+    const history = useHistory(); // Use history for navigation
 
-    const history = useHistory() // for navigation purposes
-
-    // handle movie poster being clicked
     const handleMovieContentClick = (movie) => {
-        checkTypeAndSetContent(movie)
-        history.push(`/movies/${movie.id}`)
-    }
+        const events = checkTypeAndSetContent(movie);
+        setWatchEvents(events);
+        history.push(`/movies/${movie.id}`); 
+    };
 
     return (
         <div>
-            {/* lists a movie poster for every movie in movieList */}
-            {movieList.length > 0 ? movieList.map(movie => {
-                return (
+            {/* Lists a movie poster for every movie in movieList */}
+            {movieList.length > 0 ? (
+                movieList.map(movie => (
                     <img 
                         key={movie.id} 
                         src={movie.poster_url} 
                         alt={`${movie.title} poster`} 
                         onClick={() => handleMovieContentClick(movie)} 
+                        style={{ cursor: "pointer" }}
                     />
-                )
-            }) : <p>No content found for this list. Go to Search Database tab to add content.</p> }
+                ))
+            ) : (
+                <p>No content found for this list. Go to Search Database tab to add content.</p>
+            )}
         </div>
-    )
+    );
 }
 
-export default ListMovies
+export default ListMovies;
