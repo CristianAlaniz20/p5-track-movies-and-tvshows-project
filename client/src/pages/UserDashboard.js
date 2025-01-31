0f63783a476901385aa33be5f69d609ea119e981
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-30 21:31:53 -0800

    UserDashboard.js: remove emptyContentListMessage variable from file and prop from ListMovies and ListTVShows components

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -6,33 +6,31 @@ import ListTVShows from "../components/ListTVShows";
 function UserDashboard() {
     // watched and watchlist lists for movies and tv shows from ContentStatusContext
     const { watchedMovies, watchlistMovies, watchedTVShows, watchlistTVShows } = useContext(ContentStatusContext)
-    // empty content list message
-    const emptyContentListMessage = "No content found for this list. Go to Search tab to add content."
     
     return (
         <div>
             <div>
                 {/* Display watched movies */}
                 <h2>Watched Movies</h2>
-                <ListMovies movieList={watchedMovies} emptyContentListMessage={emptyContentListMessage} />
+                <ListMovies movieList={watchedMovies} />
             </div>
 
             <div>
                 {/* Display watchlist movies */}
                 <h2>Watchlist Movies</h2>
-                <ListMovies movieList={watchlistMovies} emptyContentListMessage={emptyContentListMessage} />
+                <ListMovies movieList={watchlistMovies} />
             </div>
             
             <div>
                 {/* Display watched tv shows */}
                 <h2>Watched TV Shows</h2>
-                <ListTVShows tvShowList={watchedTVShows} emptyContentListMessage={emptyContentListMessage} />
+                <ListTVShows tvShowList={watchedTVShows} />
             </div>
 
             <div>
                 {/* Display watchlist tv shows */}
                 <h2>Watchlist TV Shows</h2>
-                <ListTVShows tvShowList={watchlistTVShows} emptyContentListMessage={emptyContentListMessage} />
+                <ListTVShows tvShowList={watchlistTVShows} />
             </div>
         </div>
     )
