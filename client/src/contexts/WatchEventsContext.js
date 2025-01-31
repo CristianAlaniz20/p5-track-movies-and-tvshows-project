772a556f973fdf22c6c59aa8fa6f11dfa12c1d5d
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-31 12:06:53 -0800

    WatchEventContext.js: add setWatchEvents to values

--- client/src/contexts/WatchEventsContext.js
+++ client/src/contexts/WatchEventsContext.js
@@ -79,6 +79,7 @@ export const WatchEventsProvider = ({ children }) => {
     return (
         <WatchEventsContext.Provider value={{
             watchEvents,
+            setWatchEvents,
             addWatchEvent,
             updateWatchEvent,
             deleteWatchEvent
