e3208504dea55b4eb4def86fb0895a2d52d1fc05
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-31 12:00:55 -0800

    WatchEventContext.js: update to include updateWatchEvent and deleteWatchEvent

--- client/src/contexts/WatchEventsContext.js
+++ client/src/contexts/WatchEventsContext.js
@@ -35,8 +35,54 @@ export const WatchEventsProvider = ({ children }) => {
         }
     };
 
+    const updateWatchEvent = async (eventId, contentId, type, status, rating, notes) => {
+        try {
+            const response = await fetch(`/${type}s/${contentId}/events/${eventId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ status, rating, notes })
+            });
+
+            if (response.ok) {
+                const updatedEvent = await response.json();
+
+                // Update user state to reflect new event details
+                setUser(prevUser => ({
+                    ...prevUser,
+                    [`${type}_watch_events`]: prevUser[`${type}_watch_events`].map(event =>
+                        event.id === updatedEvent.watch_event.id ? updatedEvent.watch_event : event
+                    )
+                }));
+            }
+        } catch (error) {
+            console.error("Error updating watch event:", error);
+        }
+    };
+
+    const deleteWatchEvent = async (eventId, contentId, type) => {
+        try {
+            const response = await fetch(`/${type}s/${contentId}/events/${eventId}`, {
+                method: 'DELETE',
+            });
+
+            if (response.ok) {
+                setUser(prevUser => ({
+                    ...prevUser,
+                    [`${type}_watch_events`]: prevUser[`${type}_watch_events`].filter(event => event.id !== eventId)
+                }));
+            }
+        } catch (error) {
+            console.error("Error deleting watch event:", error);
+        }
+    };
+
     return (
-        <WatchEventsContext.Provider value={{ watchEvents, addWatchEvent }}>
+        <WatchEventsContext.Provider value={{
+            watchEvents,
+            addWatchEvent,
+            updateWatchEvent,
+            deleteWatchEvent
+        }}>
             {children}
         </WatchEventsContext.Provider>
     );
